bdefdd09b168927d75cda8e8e27214944abe2d63
DONG <dongchuan55@gmail.com>
2016-05-25 00:19:48 +0200

    Update drawer animation

--- app/src/main/java/io/github/loop_x/yummywakeup/MainActivity.java
+++ app/src/main/java/io/github/loop_x/yummywakeup/MainActivity.java
@@ -40,38 +40,30 @@ public class MainActivity extends BaseActivity {
 
         @Override
         public void onDrawerSlide(View drawerView, float slideOffset) {
+
             View mContent = mDrawerLayout.getChildAt(0);
             View mMenu = drawerView;
+
             float scale = 1 - slideOffset;
             float rightScale = 0.8f + scale * 0.2f;
+            float leftScale = 1 - 0.3f * scale;
 
-            if (drawerView.getTag().equals("LEFT"))
-            {
+            ViewHelper.setScaleX(mMenu, leftScale);
+            ViewHelper.setScaleY(mMenu, leftScale);
 
-                float leftScale = 1 - 0.3f * scale;
-
-                ViewHelper.setScaleX(mMenu, leftScale);
-                ViewHelper.setScaleY(mMenu, leftScale);
-                ViewHelper.setAlpha(mMenu, 0.6f + 0.4f * (1 - scale));
-                ViewHelper.setTranslationX(mContent,
-                        mMenu.getMeasuredWidth() * (1 - scale));
-                ViewHelper.setPivotX(mContent, 0);
-                ViewHelper.setPivotY(mContent,
-                        mContent.getMeasuredHeight() / 2);
-                mContent.invalidate();
-                ViewHelper.setScaleX(mContent, rightScale);
-                ViewHelper.setScaleY(mContent, rightScale);
-            } else
-            {
-                ViewHelper.setTranslationX(mContent,
-                        -mMenu.getMeasuredWidth() * slideOffset);
-                ViewHelper.setPivotX(mContent, mContent.getMeasuredWidth());
-                ViewHelper.setPivotY(mContent,
-                        mContent.getMeasuredHeight() / 2);
-                mContent.invalidate();
-                ViewHelper.setScaleX(mContent, rightScale);
-                ViewHelper.setScaleY(mContent, rightScale);
+            if (mMenu.getTag() == "LEFT") {
+                ViewHelper.setTranslationX(mContent, mMenu.getMeasuredWidth() * slideOffset);
+            } else {
+                ViewHelper.setTranslationX(mContent, -mMenu.getMeasuredWidth() * slideOffset);
             }
+
+            ViewHelper.setPivotX(mContent, 0);
+            ViewHelper.setPivotY(mContent, mContent.getMeasuredHeight() / 2);
+
+            mContent.invalidate();
+
+            ViewHelper.setScaleX(mContent, rightScale);
+            ViewHelper.setScaleY(mContent, rightScale);
         }
 
         @Override
