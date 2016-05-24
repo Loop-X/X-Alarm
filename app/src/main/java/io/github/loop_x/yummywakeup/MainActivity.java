f1ca928e1273efe96bc907d959c31086b2dfdc18
DONG <dongchuan55@gmail.com>
2016-05-24 23:43:59 +0200

    Use addDrawerListener

--- app/src/main/java/io/github/loop_x/yummywakeup/MainActivity.java
+++ app/src/main/java/io/github/loop_x/yummywakeup/MainActivity.java
@@ -28,61 +28,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initListener() {
-        mDrawerLayout.setDrawerListener(new DrawerListener()
-        {
-            @Override
-            public void onDrawerStateChanged(int newState)
-            {
-            }
-
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset)
-            {
-                View mContent = mDrawerLayout.getChildAt(0);
-                View mMenu = drawerView;
-                float scale = 1 - slideOffset;
-                float rightScale = 0.8f + scale * 0.2f;
-
-                if (drawerView.getTag().equals("LEFT"))
-                {
-
-                    float leftScale = 1 - 0.3f * scale;
-
-                    ViewHelper.setScaleX(mMenu, leftScale);
-                    ViewHelper.setScaleY(mMenu, leftScale);
-                    ViewHelper.setAlpha(mMenu, 0.6f + 0.4f * (1 - scale));
-                    ViewHelper.setTranslationX(mContent,
-                            mMenu.getMeasuredWidth() * (1 - scale));
-                    ViewHelper.setPivotX(mContent, 0);
-                    ViewHelper.setPivotY(mContent,
-                            mContent.getMeasuredHeight() / 2);
-                    mContent.invalidate();
-                    ViewHelper.setScaleX(mContent, rightScale);
-                    ViewHelper.setScaleY(mContent, rightScale);
-                } else
-                {
-                    ViewHelper.setTranslationX(mContent,
-                            -mMenu.getMeasuredWidth() * slideOffset);
-                    ViewHelper.setPivotX(mContent, mContent.getMeasuredWidth());
-                    ViewHelper.setPivotY(mContent,
-                            mContent.getMeasuredHeight() / 2);
-                    mContent.invalidate();
-                    ViewHelper.setScaleX(mContent, rightScale);
-                    ViewHelper.setScaleY(mContent, rightScale);
-                }
-
-            }
-
-            @Override
-            public void onDrawerOpened(View drawerView)
-            {
-            }
-
-            @Override
-            public void onDrawerClosed(View drawerView)
-            {
-            }
-        });
+        mDrawerLayout.addDrawerListener(new DrawerListener());
     }
 
     @Override
