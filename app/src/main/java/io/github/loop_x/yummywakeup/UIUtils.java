aa137dbc717b93856cef85d7cca901dafe7da8e4
UFreedom <sunfreedom@sina.cn>
2016-06-10 11:49:39 +0800

    add util method

---
+++ app/src/main/java/io/github/loop_x/yummywakeup/UIUtils.java
@@ -0,0 +1,39 @@
+package io.github.loop_x.yummywakeup;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.WindowManager;
+
+import io.github.loop_x.yummywakeup.config.RunTime;
+
+/**
+ * Author UFreedom
+ * Date : 2016 六月 05
+ */
+public class UIUtils {
+
+    public static int getScreenWidth(){
+        DisplayMetrics dm = new DisplayMetrics();
+        WindowManager wm = (WindowManager) RunTime.getApp().getSystemService(Context.WINDOW_SERVICE);
+        wm.getDefaultDisplay().getMetrics(dm);
+        return dm.widthPixels;
+
+    }
+
+    public static int getScreenHeight(){
+        DisplayMetrics dm = new DisplayMetrics();
+        WindowManager wm = (WindowManager) RunTime.getApp().getSystemService(Context.WINDOW_SERVICE);
+        wm.getDefaultDisplay().getMetrics(dm);
+        return dm.heightPixels;
+    }
+
+    
+    public static int dip2px(float dp) {
+        Resources r = RunTime.getApp().getResources();
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics());
+        return Math.round(px);
+    }
+    
+}
