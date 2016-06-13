0f9278bd5abc058815f6502df6f97f717784580f
DONG <dongchuan55@gmail.com>
2016-06-13 20:43:07 +0200

    Add unlock type enum

---
+++ app/src/main/java/io/github/loop_x/yummywakeup/module/UnlockTypeModule/Model/UnlockTypeEnum.java
@@ -0,0 +1,62 @@
+package io.github.loop_x.yummywakeup.module.UnlockTypeModule.Model;
+
+/**
+ * Enum about all unlock types
+ */
+public enum UnlockTypeEnum {
+
+    Normal ("Normal"),
+    Math   ("Math"),
+    Puzzle ("Puzzle"),
+    Shake  ("Shake");
+
+    private final String type;
+
+    UnlockTypeEnum(String s) {
+        type = s;
+    }
+
+    public int getId(String name) {
+
+        if(name == Normal.toString()) {
+            return 0;
+        } else if(name == Math.toString()) {
+            return 1;
+        } else if(name == Puzzle.toString()) {
+            return 2;
+        } else if(name == Shake.toString()) {
+            return 3;
+        }
+
+        return -1;
+    }
+
+    public static String getName(int id) {
+
+        switch (id) {
+            case 0:
+                return Normal.toString();
+            case 1:
+                return Math.toString();
+            case 2:
+                return Puzzle.toString();
+            case 3:
+                return Shake.toString();
+        }
+
+        return null;
+    }
+
+    public String toString() {
+        return this.type;
+    }
+
+    public int toInt() {
+        return getId(type);
+    }
+
+    public boolean equalsName(String otherName) {
+        return otherName != null && type.equals(otherName);
+    }
+
+}
