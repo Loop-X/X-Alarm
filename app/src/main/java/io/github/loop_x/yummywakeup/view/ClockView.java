e8a54d821363a82bfea1b5f6591bbccbf9eddc48
UFreedom <sunfreedom@sina.cn>
2016-06-18 00:46:35 +0800

    clock view refactor code

--- app/src/main/java/io/github/loop_x/yummywakeup/view/ClockView.java
+++ app/src/main/java/io/github/loop_x/yummywakeup/view/ClockView.java
@@ -1,10 +1,8 @@
 package io.github.loop_x.yummywakeup.view;
 
 import android.content.Context;
-import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
@@ -22,18 +20,18 @@ public class ClockView extends View {
 
     private int defaultWidth;
     private int defaultHeight;
-    private int bgColor;
-    private int mWidth;
-    private int mHeight;
-    private float clockBgRadius;
-    private float clockLineRadius;
-
-    private Drawable clockBgDrawable;
-    private Drawable clockLineDrawable;
     
-    private Paint blurRingPaint;
-    private float blurRingRadius;
-    private float mRadius;
+    private int mClockWidth;
+    private int mClockHeight;
+    private float mClockRadius;
+    
+    private ClockElementDraw clockBackgroundDraw;
+    private ClockElementDraw circleBackgroundDraw;
+    private CircleLineDraw circleLineDraw;
+    private ClockElementDraw secondsIndicatorDraw;
+
+
+
 
     public ClockView(Context context) {
         this(context, null);
@@ -49,19 +47,14 @@ public class ClockView extends View {
         defaultWidth = UIUtils.dip2px(200);
         defaultHeight = UIUtils.dip2px(200);
 
-        init();
+        initClockDraw();
     }
 
-    private void init() {
-        bgColor = Color.parseColor("#1A000000");
-        clockBgDrawable = ContextCompat.getDrawable(getContext(),R.drawable.clock_bg);
-        clockLineDrawable = ContextCompat.getDrawable(getContext(),R.drawable.clock_circle_line);
-
-        blurRingPaint = new Paint();
-        blurRingPaint.setColor(Color.parseColor("#E60044"));
-        blurRingPaint.setAntiAlias(true);
-        blurRingPaint.setMaskFilter(new BlurMaskFilter(16, BlurMaskFilter.Blur.NORMAL));
-        blurRingPaint.setStyle(Paint.Style.STROKE);
+    private void initClockDraw() {
+        clockBackgroundDraw = new ClockBackgroundDraw();
+        circleBackgroundDraw = new CircleBackgroundDraw();
+        circleLineDraw = new CircleLineDraw();
+        secondsIndicatorDraw = new SecondsIndicatorDraw();
     }
 
     @Override
@@ -84,5 +77,165 @@ public class ClockView extends View {
         }
     }
 
-   
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mClockWidth = getMeasuredWidth();
+        mClockHeight = getMeasuredHeight();
+        mClockRadius = mClockWidth * 1.0f / 2;
+
+        clockBackgroundDraw.prepareDraw();
+        circleBackgroundDraw.prepareDraw();
+        circleLineDraw.prepareDraw();
+        secondsIndicatorDraw.prepareDraw();
+        
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        clockBackgroundDraw.drawSelf(canvas);
+        circleBackgroundDraw.drawSelf(canvas);
+        circleLineDraw.drawSelf(canvas);
+        secondsIndicatorDraw.drawSelf(canvas);
+    }
+    
+    
+    
+    public interface ClockElementDraw{
+        
+        public void prepareDraw();
+        
+        public void drawSelf(Canvas canvas);
+        
+    }
+    
+    public class ClockBackgroundDraw implements ClockElementDraw{
+        private int bgColor;
+
+        public ClockBackgroundDraw() {
+            bgColor = Color.parseColor("#1A000000");
+        }
+
+        @Override
+        public void prepareDraw() {
+            
+        }
+
+        @Override
+        public void drawSelf(Canvas canvas) {
+            canvas.drawColor(bgColor);
+        }
+    }
+    
+    
+    
+    public class CircleBackgroundDraw implements ClockElementDraw{
+
+        private Drawable clockBgDrawable;
+        private float translate;
+
+        public CircleBackgroundDraw(){
+            clockBgDrawable = ContextCompat.getDrawable(getContext(),R.drawable.clock_bg);
+        }
+
+
+        @Override
+        public void prepareDraw() {
+            int rightAndBottom = (int) (mClockWidth *  0.9161f);
+            float clockBgRadius = rightAndBottom * 1.0f / 2;
+            clockBgDrawable.setBounds(0,0,rightAndBottom ,rightAndBottom);
+            translate = mClockRadius - clockBgRadius;
+        }
+
+        @Override
+        public void drawSelf(Canvas canvas) {
+            canvas.save();
+            canvas.translate(translate,translate);
+            clockBgDrawable.draw(canvas);
+            canvas.restore();
+        }
+    } 
+    
+    
+    public class CircleLineDraw implements ClockElementDraw{
+        private Drawable lineDrawable;
+        private float translate;
+        private float radius;
+
+        public CircleLineDraw() {
+            lineDrawable = ContextCompat.getDrawable(getContext(),R.drawable.clock_circle_line);
+             
+        }
+        
+        public float getRadius(){
+            return radius;
+        }
+
+        @Override
+        public void prepareDraw() {
+            int rightAndBottom = (int) (mClockWidth * 0.78f);
+            lineDrawable.setBounds(0,0,rightAndBottom,rightAndBottom);
+            radius = rightAndBottom * 1.0f / 2;
+            translate = mClockRadius - radius;
+        }
+
+        @Override
+        public void drawSelf(Canvas canvas) {
+            canvas.save();
+            canvas.translate(translate,translate);
+            lineDrawable.draw(canvas);
+            canvas.restore();
+        }
+    }
+    
+    public class SecondsIndicatorDraw implements ClockElementDraw{
+        
+        private Drawable secondsIndicatorDrawable;
+        private float translate;
+        private float translateY;
+        private float angle;
+
+
+        public SecondsIndicatorDraw() {
+            secondsIndicatorDrawable = ContextCompat.getDrawable(getContext(),R.drawable.clock_night);
+        }
+
+        @Override
+        public void prepareDraw() {
+            int rightAndBottom = (int) (mClockWidth * 0.1f);
+            secondsIndicatorDrawable.setBounds(0,0,rightAndBottom,rightAndBottom);
+            float radius = rightAndBottom * 1.0f / 2;
+
+            angle = 0 / 60.0f * 360.0f;
+                    
+            translate = mClockRadius - radius;
+
+            translateY = mClockRadius - circleLineDraw.getRadius() - radius / 1.7f;
+        }
+
+        @Override
+        public void drawSelf(Canvas canvas) {
+            canvas.save();
+            canvas.rotate(angle,mClockRadius,mClockRadius);
+            canvas.translate(translate,translateY);
+            secondsIndicatorDrawable.draw(canvas);
+            
+            canvas.restore();
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
