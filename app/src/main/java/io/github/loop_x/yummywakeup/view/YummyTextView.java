3daf9ed481f37a31e893a2b850008b27ed5c25bc
UFreedom <sunfreedom@sina.cn>
2016-06-20 23:07:02 +0800

    code refactor

--- app/src/main/java/io/github/loop_x/yummywakeup/view/YummyTextView.java
+++ app/src/main/java/io/github/loop_x/yummywakeup/view/YummyTextView.java
@@ -1,9 +1,7 @@
 package io.github.loop_x.yummywakeup.view;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Typeface;
-import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
@@ -11,12 +9,12 @@ import android.widget.TextView;
 public class YummyTextView extends TextView {
 
     public YummyTextView(Context context) {
-        super(context);
+        this(context,null);
         style(context);
     }
 
     public YummyTextView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs,0);
         style(context);
     }
 
@@ -24,13 +22,7 @@ public class YummyTextView extends TextView {
         super(context, attrs, defStyleAttr);
         style(context);
     }
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public YummyTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-        style(context);
-    }
-
+    
     private void style(Context context) {
         Typeface tf = Typeface.createFromAsset(context.getAssets(),
                 "fonts/BebasNeue.otf");
