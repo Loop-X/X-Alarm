6a8bee71c22a31b2af0837016f07773efb4e913d
DONG <dongchuan55@gmail.com>
2016-07-28 22:56:42 +0200

    Add LinksHelper

---
+++ app/src/main/java/io/github/loopX/XAlarm/tools/LinksHelper.java
@@ -0,0 +1,48 @@
+package io.github.loopX.XAlarm.tools;
+
+import android.text.Spannable;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.URLSpan;
+import android.widget.TextView;
+
+public class LinksHelper {
+
+    public static void enableLinks(TextView view) {
+        if (view != null) {
+            view.setMovementMethod(LinkMovementMethod.getInstance());
+            stripUnderlines(view);
+        }
+    }
+
+    public static void stripUnderlines(TextView textView) {
+        Spannable s = (Spannable) textView.getText();
+        URLSpan[] spans = s.getSpans(0, s.length(), URLSpan.class);
+
+        for (URLSpan span: spans) {
+            int start = s.getSpanStart(span);
+            int end = s.getSpanEnd(span);
+            s.removeSpan(span);
+            span = new URLSpanNoUnderline(span.getURL());
+            s.setSpan(span, start, end, 0);
+        }
+
+        textView.setText(s);
+    }
+
+    // Customized URLSpan which has no under line
+    private static class URLSpanNoUnderline extends URLSpan {
+
+        public URLSpanNoUnderline(String url) {
+            super(url);
+        }
+
+        @Override
+        public void updateDrawState(TextPaint ds) {
+            super.updateDrawState(ds);
+            ds.setUnderlineText(false);
+        }
+
+    }
+
+}
