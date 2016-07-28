02dea2dda4e8e2aba8835295e399cfa97cbcffdb
DONG <dongchuan55@gmail.com>
2016-07-28 22:49:02 +0200

    Update about activity

--- app/src/main/java/io/github/loopX/XAlarm/About.java
+++ app/src/main/java/io/github/loopX/XAlarm/About.java
@@ -1,17 +1,20 @@
 package io.github.loopX.XAlarm;
 
+import android.support.v7.widget.Toolbar;
+
 import io.github.loopX.XAlarm.infrastructure.BaseActivity;
 
 public class About extends BaseActivity{
 
     @Override
     public int getLayoutId() {
-        return 0;
+        return R.layout.activity_about;
     }
 
     @Override
     public void onViewInitial() {
-
+        Toolbar bar = (Toolbar) findViewById(R.id.tb_about);
+        setSupportActionBar(bar);
     }
 
     @Override
