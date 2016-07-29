b611912a4d70e7e99aa9efafd90118e4c42db72b
DONG <dongchuan55@gmail.com>
2016-07-29 22:19:07 +0200

    Add send email button

--- app/src/main/java/io/github/loopX/XAlarm/About.java
+++ app/src/main/java/io/github/loopX/XAlarm/About.java
@@ -1,6 +1,11 @@
 package io.github.loopX.XAlarm;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -9,6 +14,8 @@ import io.github.loopX.XAlarm.tools.LinksHelper;
 
 public class About extends BaseActivity{
 
+    private ImageView ivSendEmail;
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_about;
@@ -16,6 +23,8 @@ public class About extends BaseActivity{
 
     @Override
     public void onViewInitial() {
+
+        ivSendEmail = (ImageView) findViewById(R.id.iv_send_email);
 
         // Set toolbar
         Toolbar bar = (Toolbar) findViewById(R.id.tb_about);
@@ -27,6 +36,16 @@ public class About extends BaseActivity{
             TextView child = (TextView) linksHolder.getChildAt(i);
             LinksHelper.enableLinks(child);
         }
+
+        ivSendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                        "mailto","email@email.com", null));
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Contact Loop-X");
+                startActivity(Intent.createChooser(intent, "Choose an Email client :"));
+            }
+        });
     }
 
     @Override
