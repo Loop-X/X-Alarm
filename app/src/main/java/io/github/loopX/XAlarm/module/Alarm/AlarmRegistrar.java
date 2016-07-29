d22c9878a1e058f9455878bd27b2f7d24910dd20
DONG <dongchuan55@gmail.com>
2016-07-29 23:29:01 +0200

    Add alarm registrar

---
+++ app/src/main/java/io/github/loopX/XAlarm/module/Alarm/AlarmRegistrar.java
@@ -0,0 +1,42 @@
+package io.github.loopX.XAlarm.module.Alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import java.util.List;
+
+import io.github.loopX.XAlarm.database.AlarmDBService;
+import io.github.loopX.XAlarm.model.Alarm;
+
+/**
+ * This BroadcastReceiver is registered to be called for the following system intents:
+ *
+ * BOOT_COMPLETED, TIMEZONE_CHANGED, TIME_SET, DATE_CHANGED
+ */
+public class AlarmRegistrar extends BroadcastReceiver {
+
+    private static void refreshAlarms(Context context) {
+        AlarmDBService.getInstance(context).getAlarms();
+
+        List<Alarm> alarms = AlarmDBService.getInstance(context).getAlarms();
+
+        Alarm mAlarm = null;
+
+        if(alarms.isEmpty()) {
+            mAlarm = new Alarm();
+            AlarmDBService.getInstance(context).addAlarm(mAlarm);
+        } else {
+            // Always force to get first alarm in list
+            mAlarm = alarms.get(0);
+        }
+
+        mAlarm.cancel();
+        mAlarm.schedule();
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        refreshAlarms(context);
+    }
+}
