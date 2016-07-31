457c8f42850ada9c0f656bc0383e74fdee1ebd0b
DONG <dongchuan55@gmail.com>
2016-07-31 17:54:54 +0200

    remove useless files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,191 +1,150 @@
 /*
-	Overflow by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+ Overflow by HTML5 UP
+ html5up.net | @ajlkn
+ Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+ */
 
-(function($) {
+(function ($) {
 
-	var settings = {
+    var settings = {
 
-		// Full screen header?
-			fullScreenHeader: true,
+        // Full screen header?
+        fullScreenHeader: true,
 
-		// Parallax background effect?
-			parallax: true,
+        // Parallax background effect?
+        parallax: true,
 
-		// Parallax factor (lower = more intense, higher = less intense).
-			parallaxFactor: 10
+        // Parallax factor (lower = more intense, higher = less intense).
+        parallaxFactor: 10
 
-	};
+    };
 
-	skel.breakpoints({
-		wide: '(max-width: 1680px)',
-		normal: '(max-width: 1080px)',
-		narrow: '(max-width: 840px)',
-		mobile: '(max-width: 736px)'
-	});
+    skel.breakpoints({
+        wide: '(max-width: 1680px)',
+        normal: '(max-width: 1080px)',
+        narrow: '(max-width: 840px)',
+        mobile: '(max-width: 736px)'
+    });
 
-	$(function() {
+    $(function () {
 
-		var	$window = $(window),
-			$body = $('body');
+        var $window = $(window),
+            $body = $('body');
 
-		if (skel.vars.touch) {
+        if (skel.vars.touch) {
+            settings.parallax = false;
+            $body.addClass('is-scroll');
 
-			settings.parallax = false;
-			$body.addClass('is-scroll');
+        }
 
-		}
+        // Disable animations/transitions until the page has loaded.
+        $body.addClass('is-loading');
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+        $window.on('load', function () {
+            $body.removeClass('is-loading');
+        });
 
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
+        // CSS polyfills (IE<9).
+        if (skel.vars.IEVersion < 9)
+            $(':last-child').addClass('last-child');
 
-		// CSS polyfills (IE<9).
-			if (skel.vars.IEVersion < 9)
-				$(':last-child').addClass('last-child');
+        // Fix: Placeholder polyfill.
+        $('form').placeholder();
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+        // Prioritize "important" elements on mobile.
+        skel.on('+mobile -mobile', function () {
+            $.prioritize(
+                '.important\\28 mobile\\29',
+                skel.breakpoint('mobile').active
+            );
+        });
 
-		// Prioritize "important" elements on mobile.
-			skel.on('+mobile -mobile', function() {
-				$.prioritize(
-					'.important\\28 mobile\\29',
-					skel.breakpoint('mobile').active
-				);
-			});
+        // Scrolly links.
+        $('.scrolly-middle').scrolly({
+            speed: 1000,
+            anchor: 'middle'
+        });
 
-		// Scrolly links.
-			$('.scrolly-middle').scrolly({
-				speed: 1000,
-				anchor: 'middle'
-			});
+        $('.scrolly').scrolly({
+            speed: 1000,
+            offset: function () {
+                return (skel.breakpoint('mobile').active ? 70 : 190);
+            }
+        });
 
-			$('.scrolly').scrolly({
-				speed: 1000,
-				offset: function() { return (skel.breakpoint('mobile').active ? 70 : 190); }
-			});
+        // Full screen header.
+        if (settings.fullScreenHeader) {
 
-		// Full screen header.
-			if (settings.fullScreenHeader) {
+            var $header = $('#header');
 
-				var $header = $('#header');
+            if ($header.length > 0) {
 
-				if ($header.length > 0) {
+                var $header_header = $header.find('header');
 
-					var $header_header = $header.find('header');
+                $window
+                    .on('resize.overflow_fsh', function () {
 
-					$window
-						.on('resize.overflow_fsh', function() {
+                        if (skel.breakpoint('mobile').active)
+                            $header.css('padding', '');
+                        else {
 
-							if (skel.breakpoint('mobile').active)
-								$header.css('padding', '');
-							else {
+                            var p = Math.max(192, ($window.height() - $header_header.outerHeight()) / 2);
+                            $header.css('padding', p + 'px 0 ' + p + 'px 0');
 
-								var p = Math.max(192, ($window.height() - $header_header.outerHeight()) / 2);
-								$header.css('padding', p + 'px 0 ' + p + 'px 0');
+                        }
 
-							}
+                    })
+                    .trigger('resize.overflow_fsh');
 
-						})
-						.trigger('resize.overflow_fsh');
+                $window.load(function () {
+                    $window.trigger('resize.overflow_fsh');
+                });
 
-					$window.load(function() {
-						$window.trigger('resize.overflow_fsh');
-					});
+            }
 
-				}
+        }
 
-			}
+        // Parallax background.
 
-		// Parallax background.
+        // Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+        if (skel.vars.browser == 'ie'
+            || skel.vars.mobile)
+            settings.parallax = false;
 
-			// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-				if (skel.vars.browser == 'ie'
-				||	skel.vars.mobile)
-					settings.parallax = false;
+        if (settings.parallax) {
 
-			if (settings.parallax) {
+            var $dummy = $(), $bg;
 
-				var $dummy = $(), $bg;
+            $window
+                .on('scroll.overflow_parallax', function () {
 
-				$window
-					.on('scroll.overflow_parallax', function() {
+                    // Adjust background position.
+                    $bg.css('background-position', 'center ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
 
-						// Adjust background position.
-							$bg.css('background-position', 'center ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
+                })
+                .on('resize.overflow_parallax', function () {
 
-					})
-					.on('resize.overflow_parallax', function() {
+                    // If we're in a situation where we need to temporarily disable parallax, do so.
+                    if (!skel.breakpoint('wide').active
+                        || skel.breakpoint('narrow').active) {
 
-						// If we're in a situation where we need to temporarily disable parallax, do so.
-							if (!skel.breakpoint('wide').active
-							||	skel.breakpoint('narrow').active) {
+                        $body.css('background-position', '');
+                        $bg = $dummy;
 
-								$body.css('background-position', '');
-								$bg = $dummy;
+                    }
 
-							}
+                    // Otherwise, continue as normal.
+                    else
+                        $bg = $body;
 
-						// Otherwise, continue as normal.
-							else
-								$bg = $body;
+                    // Trigger scroll handler.
+                    $window.triggerHandler('scroll.overflow_parallax');
 
-						// Trigger scroll handler.
-							$window.triggerHandler('scroll.overflow_parallax');
+                })
+                .trigger('resize.overflow_parallax');
 
-					})
-					.trigger('resize.overflow_parallax');
+        }
 
-			}
-
-		// Poptrox.
-			$('.gallery').poptrox({
-				useBodyOverflow: false,
-				usePopupEasyClose: false,
-				overlayColor: '#0a1919',
-				overlayOpacity: (skel.vars.IEVersion < 9 ? 0 : 0.75),
-				usePopupDefaultStyling: false,
-				usePopupCaption: true,
-				popupLoaderText: '',
-				windowMargin: 10,
-				usePopupNav: true
-			});
-
-	});
-
-	$("#submit").click(function() {
-
-		var name = $("#name").val();
-		var email = $("#email").val();
-		var message = $("#message").val();
-
-		// To empty previous error/success message.
-		$("#return-message").empty();
-
-		// Checking for blank fields.
-		if (name == '' || email == '' || contact == '') {
-			alert("Please Fill Required Fields");
-		} else {
-
-		// Returns successful data submission message when the entered information is stored in database.
-			$.post("contact_form.php", {
-				name1: name,
-				email1: email,
-				message1: message,
-				contact1: contact
-			}, function(data) {
-				$("#returnmessage").append(data); // Append returned message to message paragraph.
-				if (data == "Your Query has been received, We will contact you soon.") {
-					$("#form")[0].reset(); // To reset form fields on success.
-				}
-			});
-		}
-	});
+    });
 
 })(jQuery);
