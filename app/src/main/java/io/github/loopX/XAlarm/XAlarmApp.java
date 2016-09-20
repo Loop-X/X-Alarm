d3bdf889fb0c945794591f10ed3cfa2c5c98f1d7
UFreedom <sunfreedom@sina.cn>
2016-09-21 00:05:14 +0800

    make the way of get 'talking data" app id more secure

--- app/src/main/java/io/github/loopX/XAlarm/XAlarmApp.java
+++ app/src/main/java/io/github/loopX/XAlarm/XAlarmApp.java
@@ -2,18 +2,22 @@ package io.github.loopX.XAlarm;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
 
 import com.tendcloud.tenddata.TCAgent;
 
 
 /**
  * Specialization of the Application class to enable:
- *      Application context access from non-Android framework classes
- *      Package Name access from non-Android framework classes
- *      Resource Path access from non-Android framework classes
+ * Application context access from non-Android framework classes
+ * Package Name access from non-Android framework classes
+ * Resource Path access from non-Android framework classes
  */
-public class XAlarmApp extends Application{
+public class XAlarmApp extends Application {
 
+    private static final String TAG = "XAlarmApp";
     private static Context mContext;
     private static String mPackageName;
 
@@ -36,9 +40,21 @@ public class XAlarmApp extends Application{
         XAlarmApp.mPackageName = XAlarmApp.mContext.getPackageName();
 
         if (!BuildConfig.DEBUG) {
-            TCAgent.LOG_ON = true;
-            TCAgent.init(this, BuildConfig.TALKING_DATA_ID, BuildConfig.CHANNEL);
-            TCAgent.setReportUncaughtExceptions(true);
+            try {
+                ApplicationInfo appInfo = getPackageManager()
+                        .getApplicationInfo(getPackageName(),
+                                PackageManager.GET_META_DATA);
+                String appId = appInfo.metaData.getString("TD_APP_ID");
+                if(appId != null && !"".equals(appId)){
+                    TCAgent.LOG_ON = true;
+                    TCAgent.init(this, appId, BuildConfig.CHANNEL);
+                    TCAgent.setReportUncaughtExceptions(true);
+                    Log.e(TAG, "talking data app id:" + appId);
+                }
+            } catch (PackageManager.NameNotFoundException e) {
+                Log.e(TAG, "Can not get talking data app id");
+                e.printStackTrace();
+            }
         }
     }
 }
